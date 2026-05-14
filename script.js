d9663efa8b8910a22dee3c22592a27b09f987e66
sampathsrigada <sampathsrigada10@gmail.com>
2026-05-14 11:38:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let boxes = document.getElementsByClassName("square")
+
+for(let box in boxes){
+	box.addEventListener("mouseenter", ()=>{
+		for(let item of boxes){
+			if(item!== box){
+				item.classList.add("active")
+			}
+		}
+	})
+	box.addEventListener("mouseout", ()=>{
+		for(let item of boxes){
+			if(item!== box){
+				item.classList.remove("active")
+			}
+		}
+	})	
+}
+
